1e804c1e9c207341fa9f931747ad080c6214afa5
Kishan Joshi <robokishan.blogspot@gmail.com>
2022-03-10 22:49:51 +0530

    add: init.sh

---
+++ init.sh
@@ -0,0 +1,17 @@
+BASE_HOME=/etc
+DONE_FILE=$BASE_HOME/DONE
+
+if [ -s $DONE_FILE ]
+then
+    echo "RESTART NOT REQUIRED"   
+else
+    sudo bash /home/pi/seeed-voicecard/install.sh
+    status=$?
+    if [ $status -eq 0 ]
+    then
+        echo "Installation Successful"
+        echo 1 > $DONE_FILE
+    else 
+        echo "Command installation Unsuccessful"
+    fi
+fi
